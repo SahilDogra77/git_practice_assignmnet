b3cf32949a1bc498a6bf332cb36bebe8c7f2a8a5
Sahildogra77 <sahildogra233@gmail.com>
2022-09-18 23:15:22 +0530

    Palidrome and Prime Problem

---
+++ Palidrome.js
@@ -0,0 +1,31 @@
+n="abba"
+
+flag=0;
+
+
+let str="abba"
+
+for(i=0;i<=n-1;i++)
+
+  {
+ 
+if(str[i]==n[i])
+ 
+    flag=1;
+
+  }
+
+if(flag==1)
+
+{
+ 
+console.log("Palidrome");
+
+}
+
+else
+{
+
+  console.log("Not Palidrome");
+
+}
---
+++ Prime.js
@@ -0,0 +1,29 @@
+let n=13;
+
+
+for(i=1;i<=n;i++)
+  
+{
+
+  if(n%i==0)
+
+  count++;
+  
+}
+
+
+if(count==2)
+
+{
+
+  console.log("Yes");
+
+}
+
+
+else
+{
+
+ console.log("No");
+
+}
